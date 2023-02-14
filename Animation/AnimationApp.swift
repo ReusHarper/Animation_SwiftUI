00078c2555f5b709b5246fabbb6ee057894c3876
ReusHarper <reusharper@comunidad.unam.mx>
2023-02-13 20:55:44 -0600

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by ReusHarper on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
